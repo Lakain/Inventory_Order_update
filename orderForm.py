babf2b5d26adf540e8aa3ca413007dbcd122ba61
Lakain <60016343+Lakain@users.noreply.github.com>
2023-11-29 17:21:39 -0600

    minor changes
    - Order-id added to OrderForm & Order History

--- orderForm.py
+++ orderForm.py
@@ -8,18 +8,18 @@ from pandasModel import PandasModel
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import  QSortFilterProxyModel
 
-# root_path = "Z:/excel files/00 RMH Sale report/"
-root_path = ''
+root_path = "Z:/excel files/00 RMH Sale report/"
+# root_path = ''
 
 class orderForm(QWidget):
     def __init__(self, order_list:list, df: pd.DataFrame):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        df = df[['sku','ORD', 'DESCRIPTION']]
+        df = df[['sku','ORD', 'DESCRIPTION', 'order-id']]
         df['ORD'] = pd.to_numeric(df['ORD'], downcast='integer')
 
-        self.model = PandasModel(df[df['sku'].isin(order_list)].groupby(['sku'],as_index=False).sum())
+        self.model = PandasModel(df[df['sku'].isin(order_list)])
         self.proxymodel = QSortFilterProxyModel()
         self.proxymodel.setSourceModel(self.model)
         self.ui.tableView.setModel(self.proxymodel)
@@ -31,16 +31,17 @@ class orderForm(QWidget):
         new_history = self.model._data[['sku']]
         new_history['order date'] = datetime.date.today().strftime("%m/%d/%Y")
         new_history['qty'] = self.model._data[['ORD']]
+        new_history['order-id'] = self.model._data['order-id']
         history = pd.concat([history, new_history], ignore_index=True)
         history.to_excel(root_path+'appdata/order_history.xlsx', index=False)
         print(history)
         self.model._data.style.set_properties(border="thin solid black").to_excel(root_path+'appdata/orderForm.xlsx', index=False, engine='openpyxl', startrow=2)
         
-        # workbook= openpyxl.load_workbook(root_path+'/appdata/orderForm.xlsx')
-        workbook= openpyxl.load_workbook(os.getcwd()+'/appdata/orderForm.xlsx')
+        workbook= openpyxl.load_workbook(root_path+'/appdata/orderForm.xlsx')
+        # workbook= openpyxl.load_workbook(os.getcwd()+'/appdata/orderForm.xlsx')
         worksheet = workbook.get_sheet_by_name('Sheet1')
         worksheet['A1'] = datetime.date.today().strftime("%m/%d/%y") +" 7 MILE (651-290-0362)"
         workbook.save(root_path+'appdata/orderForm.xlsx')
         
-        # os.system('"'+root_path+'/appdata/orderForm.xlsx"')
-        os.system('"'+os.getcwd()+'/appdata/orderForm.xlsx"')
+        os.system('"'+root_path+'/appdata/orderForm.xlsx"')
+        # os.system('"'+os.getcwd()+'/appdata/orderForm.xlsx"')
